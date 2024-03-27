4565d0efd20f742b3313a76b7051b258f753080c
HB <64822529+hbsgithub@users.noreply.github.com>
2024-03-27 14:13:33 +0800

    Update media-check.js

--- media-check.js
+++ media-check.js
@@ -356,7 +356,7 @@ function nfTest(filmId) {
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
             }
         }
-        $task.fetch(params).then(response => {
+        $httpClient.get(params, (errormsg,response,data) => {
             console.log("----------NetFlix--------------");
             if (errormsg) {
                 console.log("NF request failed:" + errormsg);
